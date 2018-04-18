cc782931f896970ecf44e336b88e98f004a2b82f
Pablo <>
2018-04-18 11:09:45 +0200

    swing

--- ConvertirCelsius/src/convertircelsius/ConvertirCelsius.java
+++ ConvertirCelsius/src/convertircelsius/ConvertirCelsius.java
@@ -6,6 +6,7 @@
 package convertircelsius;
 
 import java.awt.AWTEvent;
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -14,6 +15,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
@@ -29,7 +31,7 @@ public class ConvertirCelsius {
         // TODO code application logic here
         JFrame ventana = new JFrame("Convertidor Celsius");
         Container cpane = ventana.getContentPane();
-        ventana.setLayout(new FlowLayout());
+        //ventana.setLayout(new FlowLayout());
         /*GroupLayout layout = new GroupLayout(cpane);
         cpane.setLayout(layout);*/
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -42,13 +44,17 @@ public class ConvertirCelsius {
         
         
         campoTexto.setPreferredSize(new Dimension(50, 30));
+        JPanel panelN = new JPanel();
+        JPanel panelC = new JPanel();
         
         
-        cpane.add(campoTexto);
-        cpane.add(etiquetaC);
-        cpane.add(convertir);
-        cpane.add(etiquetaF);
-        cpane.add(limpiar);
+        panelN.add(campoTexto);
+        panelN.add(etiquetaC);
+        panelC.add(convertir);
+        panelC.add(etiquetaF);
+        cpane.add(limpiar, BorderLayout.SOUTH);
+        cpane.add(panelN,BorderLayout.NORTH);
+        cpane.add(panelC,BorderLayout.CENTER);
         ventana.setBounds(500, 300, 400, 200);
         
         ventana.setVisible(true);
